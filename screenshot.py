75494d51040cc05802a481d305910a60a105091c
Kaiserchen85 <kaiser.chen85@gmail.com>
2026-01-17 23:41:46 -0800

    attempt at screenshotting just the screen

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,5 @@
 import pyautogui
+import pywinctl as pwc
 import os
 import time
 
@@ -14,8 +15,42 @@ def capture_and_save_to_out(filename):
         os.makedirs(base_dir)
         print(f"Created directory: {base_dir}")
 
+    all_titles = pwc.getAllTitles()
+
+    print("--- ACTIVE WINDOW TITLES ---")
+    for title in all_titles: #<- prints out all of them, find the right name
+        if title.strip():  # Filters out empty strings
+            print(f"'{title}'")
+
+    windows = pwc.getWindowsWithTitle("Chrome") # for this, fix it
+    # Key Parameters
+# title: The string you are looking for.
+
+# condition: (Optional) Defines how the search matches the title.
+
+    # pwc.Re.CONTAINS (Default): Matches if the string is anywhere in the title.
+
+    # pwc.Re.STARTSWITH: Matches if the title starts with the string.
+
+    # pwc.Re.ENDSWITH: Matches if the title ends with the string.
+
+    # pwc.Re.EQUALS: Requires an exact match.
+
+# scope: (Optional) You can limit the search to specific types of windows (e.g., only visible windows).
+    
+    region = None
+    if windows:
+        win = windows[0]
+        # 2. These values update if you move/resize the app!
+        region = (win.left, win.top, win.width, win.height)
+    
+    if region is None:
+        print("Failed to capture...")
+        return
+
+    time.sleep(0.5)
     # 4. Take the screenshot
-    screenshot = pyautogui.screenshot()
+    screenshot = pyautogui.screenshot(region=region)
 
     # 5. Save it
     screenshot.save(full_path)
@@ -24,6 +59,8 @@ def capture_and_save_to_out(filename):
     return full_path
 
 # Example Usage:
+i = 0
 while(True):
-    time.sleep(10)
-    path_to_image = capture_and_save_to_out("screenshot.jpeg")
+    time.sleep(5)
+    path_to_image = capture_and_save_to_out(f"screenshot_{i}.jpeg")
+    i += 1
